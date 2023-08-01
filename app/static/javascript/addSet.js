60f0843444327bf6bfc01e0a5b980e18740cb9f6
Bronson Wardle <bronson.wardle@gmail.com>
2023-07-31 20:15:30 -0400

    added add set button under weight and reps. Started code for functionallity for the add set. Was able to get a clone of the weight and reps but it neds to be posistioned better and return the new values of nothing

--- app/static/javascript/addSet.js
+++ app/static/javascript/addSet.js
@@ -1,2 +1,21 @@
 // When user clicks + button a new set of weight and reps are populated.
 // when button click a new sent of LI is populated.
+
+function addSet () {
+
+  const weightElement = document.querySelector('.card-weight');
+  const repsElement = document.querySelector('.card-reps');
+
+  const newWeightElement = weightElement.cloneNode(true);
+  const newRepsElement = repsElement.cloneNode(true);
+
+  // Reset input values in the cloned elements
+  newWeightElement.querySelector('input').value = '';
+  newRepsElement.querySelector('input').value = '';
+
+  const exerciseList = document.getElementById('exercise-list');
+  exerciseList.appendChild(newWeightElement);
+  exerciseList.appendChild(newRepsElement);
+}
+
+document.getElementById("addSet").onclick = addSet;
