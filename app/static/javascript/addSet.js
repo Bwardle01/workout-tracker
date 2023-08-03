b0904c7b1132032364cb3f6eb1cdc5aeab9d1fc6
Bronson Wardle <bronson.wardle@gmail.com>
2023-08-02 20:27:04 -0400

    Made changes to the Styling of weight and reps. Also change a bit of the add set as well. Still trying to figure out how this is going to work wit the add set stuff

--- app/static/javascript/addSet.js
+++ app/static/javascript/addSet.js
@@ -1,21 +1,20 @@
 // When user clicks + button a new set of weight and reps are populated.
 // when button click a new sent of LI is populated.
 
-function addSet () {
+function addSet() {
+	const weightElement = document.getElementById("weightInput");
+	const repsElement = document.getElementById("repsInput");
 
-  const weightElement = document.querySelector('.card-weight');
-  const repsElement = document.querySelector('.card-reps');
+	const newWeightElement = weightElement.cloneNode(true);
+	const newRepsElement = repsElement.cloneNode(true);
 
-  const newWeightElement = weightElement.cloneNode(true);
-  const newRepsElement = repsElement.cloneNode(true);
+	// Reset values on click
+	newWeightElement.value = "";
+	newRepsElement.value = "";
 
-  // Reset values on click
-  newWeightElement.querySelector('input').value = '';
-  newRepsElement.querySelector('input').value = '';
-
-  const exerciseList = document.getElementById('exercise-list');
-  exerciseList.appendChild(newWeightElement);
-  exerciseList.appendChild(newRepsElement);
+	const exerciseList = document.querySelector(".weight-reps-container");
+	exerciseList.appendChild(newWeightElement);
+	exerciseList.appendChild(newRepsElement);
 }
 
 document.getElementById("addSet").onclick = addSet;
