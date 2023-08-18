583518a7ee567dd91ab29d2a39dd0a5424264453
unknown <bronson.wardle@gmail.com>
2023-08-17 20:18:19 -0400

    Created add exercise function to add another exercise when the add exercise button is clicked. The fundtion works but we need to learn how to display it under the first exercise.

--- app/static/javascript/addExercise.js
+++ app/static/javascript/addExercise.js
@@ -1,6 +1,33 @@
 // When a user clicks add exercise, a new exercise field will be populated along with weight and reps.
 // When a new exercise is populated a + button is also added to add a new set.
+function addExercise() { 
 
+	const weightElement = document.getElementById("weightInput");
+	const repsElement = document.getElementById("repsInput");
+  const exerciseElement = document.getElementById("exerciseInput");
+  const setElement = document.getElementById("addSet")
+
+	const newWeightElement = weightElement.cloneNode(true);
+	const newRepsElement = repsElement.cloneNode(true);
+  const newExerciseElement = exerciseElement.cloneNode(true);
+  const newSetElement = setElement.cloneNode(true);
+
+  newWeightElement.value = "";
+	newRepsElement.value = "";
+  newExerciseElement.value = "";
+
+  const exerciseList = document.querySelector(".weight-reps-container");
+  const exerciseContainer = document.querySelector(".exercise-container");
+  const addSetContainer = document.querySelector(".add-set-container");
+
+
+	exerciseList.appendChild(newWeightElement);
+	exerciseList.appendChild(newRepsElement);
+  exerciseContainer.appendChild(newExerciseElement);
+  addSetContainer.appendChild(newSetElement);
+};
+
+document.getElementById('addExercise').onclick = addExercise;
 
 
 
